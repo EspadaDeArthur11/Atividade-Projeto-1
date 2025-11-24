91c1626f6b68ba8364232f5cfe49b9fe3c75e237
grmsBR <guilherme.ramos.co@gmail.com>
2025-11-23 23:25:52 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -2,6 +2,7 @@
 """
 """
 from grafoMatriz import GrafoRot
+import api_igdb
 
 def main():
     grafo = GrafoRot(1)
@@ -15,7 +16,7 @@ def main():
         print("Opção 0 para ler ou construa manualmente.\n")
 
     while not terminar:
-        print("Menu")
+        print("LudoRadar - Menu de Opções:")
         print("0: Ler dados do arquivo grafo.txt")
         print("1: Gravar dados no arquivo grafo.txt")
         print("2: Inserir vértice")
@@ -25,7 +26,11 @@ def main():
         print("6: Mostrar conteúdo do arquivo")
         print("7: Mostrar grafo")
         print("8: Apresentar conexidade do grafo")
-        print("9: Encerrar o programa")
+        print("9: Montar árvore geradora máxima do grafo")
+        print("10: Mostrar todos os caminhos mínimos do grafo")
+        print("11: Verificar se o grafo é euleriano")
+        print("12: Recomendar jogos com base em um jogo específico")
+        print("13: Encerrar o programa")
 
         try:
             inp = int(input("Escolha uma opção: ").strip())
@@ -37,9 +42,9 @@ def main():
             case 0:
                 try:
                     grafo.makeGraphFromFile("grafo.txt")
-                    print("grafo.txt lido e grafo montado.\n")
-                except FileNotFoundError:
-                    print("Arquivo grafo.txt não encontrado.\n")
+                    print("grafo.txt carregado com sucesso.\n")
+                except Exception:
+                    print("Opção 0 para ler ou construa manualmente.\n")
 
             case 1:
                 grafo.makeFileFromGraph("grafo.txt")
@@ -110,9 +115,42 @@ def main():
                     print("Desconexo")
                 else:
                     print("Conexo")
-               
 
             case 9:
+                agm = grafo.prim()
+                print("Custo máximo: ", agm[0])
+                grafo2 = GrafoRot(grafo.vertices)
+                for i in agm[1]:
+                    grafo2.insereA(i[0], i[1], i[2])
+                grafo2.show(omitir_infinito=True)
+
+            case 10:
+                grafo.Floyd()
+
+            case 11:
+                if grafo.caminhoEuleriano():
+                    print("O grafo é euleriano.")
+                else:
+                    print("O grafo não é euleriano.")
+
+            case 12:
+                print("Insira o nome do jogo para recomendação:")
+                nome_jogo = input().lower()
+                for i in grafo.rot:
+                    jogo = i[0].lower()
+                    for simb in [".", ",", ":", ";", "!", "?", "\"", "'", " ", "-", "_"]:
+                        jogo = jogo.replace(simb, "")
+                        nome_jogo = nome_jogo.replace(simb, "")
+                    if nome_jogo == jogo:
+                        indice_jogo = grafo.rot.index(i)
+                        #print(indice_jogo)
+                        print(f"Recomendações para '{i[0]}':")
+                        for v in range(grafo.vertices):
+                            if grafo.adj[indice_jogo][v] >= 5 and indice_jogo != v:
+                                print(f"- {grafo.rot[v][0]} (Peso da aresta: {grafo.adj[indice_jogo][v]})")
+                        break
+                
+            case 13:
                 terminar = True
 
             case _:
